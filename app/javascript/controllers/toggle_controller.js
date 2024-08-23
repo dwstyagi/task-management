49e2e3a6c972edd267c5e14a3524c359fc839a95
Diwanshu Tyagi <dwstyagi@gmail.com>
2024-08-23 20:44:18 +0530

    add sidebar

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,10 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["profileCard"]
+  static targets = ["profileCard", "sidebar"]
 
   toggleProfileCard(){
     this.profileCardTarget.classList.toggle("hidden")
+  }
+
+  toggleSidebar(){
+    this.sidebarTarget.classList.toggle("hidden")
   }
 
 }
